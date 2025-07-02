0001bd54438ddb9df4c5bfa23242ddb8d0a20650
Wouter Lagerweij <wouter@lagerweij.com>
2025-07-02 16:59:11 +0200

    TypeScript empty project

---
+++ src/VendingMachine.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+
+class VendingMachine {
+}
+
+export default VendingMachine;
---
+++ test/VendingMachine.test.ts
@@ -0,0 +1,10 @@
+import "jest";
+import VendingMachine from "../src/VendingMachine";
+
+describe("VendingTest", () => {
+    it("Does something", () => {
+        const vendingMachine = new VendingMachine();
+        expect(true).toBe(false);
+    });
+
+});
